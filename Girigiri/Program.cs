0a15a5e43d9284c53f99706ec94a809c639a9c92
UlyssesWu <wdwxy12345@gmail.com>
2018-12-28 22:09:21 +0800

    Furikiri.Tools.Disassembler
    我觉得这个名字不够毒，于是改成现在这个名字了。这回有毒吧

---
+++ Girigiri/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Furikiri.Emit;
+
+namespace Furikiri.Girigiri
+{
+    class Program
+    {
+        private static Assembler _asm = new Assembler();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Furikiri TJS2 Disassembler");
+            Console.WriteLine("by Ulysses, wdwxy12345@gmail.com");
+            Console.WriteLine();
+
+            if (args.Length <= 0)
+            {
+                PrintHelp();
+                return;
+            }
+
+            foreach (string s in args)
+            {
+                if (Directory.Exists(s)) //disasm dir
+                {
+                    var list = Directory.EnumerateFiles(s, "*.tjs").Where(ss => ss.ToLowerInvariant().EndsWith(".tjs"))
+                        .ToList();
+                    foreach (var p in list)
+                    {
+                        Disassemble(p);
+                    }
+                }
+                else if (File.Exists(s))
+                {
+                    Disassemble(s);
+                }
+            }
+
+            Console.WriteLine("All done!");
+        }
+
+        private static void Disassemble(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                return;
+            }
+            Console.Write($"Process {path} ...");
+            try
+            {
+                var result = _asm.Disassemble(new Module(path));
+                File.WriteAllText(Path.ChangeExtension(path, ".tjsasm"), result);
+                Console.WriteLine("Done.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed!");
+            }
+        }
+
+        private static void PrintHelp()
+        {
+            Console.WriteLine(@"Usage: .exe <TJS2 dir or file>
+The decompile feature is in dev.");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 
 Furikiri (FreeKiri / [フリキリ](https://ja.wikipedia.org/wiki/%E3%83%95%E3%83%AA%E3%82%AF%E3%83%AA#OVA)) is a managed TJS2 disassembler/decompiler.
 
+## Furikiri.Girigiri (In Dev)
+*ギリギリ愛♫~ キリキリ舞い♫~*
+
+Managed TJS2 bytecode disassembler/decompiler. Does not require KRKR engine (KRKR 2/Z/#) to run.
+
 ---
 
 by Ulysses (wdwxy12345@gmail.com) from Project AZUSA 
 
-Furikiri is licensed under LGPLv3.
+Furikiri is licensed under **LGPLv3**.
